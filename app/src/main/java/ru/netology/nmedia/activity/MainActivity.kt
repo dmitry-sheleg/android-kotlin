ce6a5f4361d9a3efedf398dd29c41ff1e7ca1dee
Dmitry Sheleg <dimdim1999@mail.ru>
2026-06-01 16:42:33 +0300

    New branch crud, added menu Edit and Remove

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,16 +1,22 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,18 +29,57 @@ class MainActivity : AppCompatActivity() {
         }
 
         val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
 
-        val adapter = PostsAdapter(
-            onLikeListener = { post ->
+            override fun onLike(post: Post) {
                 viewModel.likeById(post.id)
-            },
-            onShareListener = { post ->
+            }
+
+            override fun onShare(post: Post) {
                 viewModel.shareById(post.id)
             }
-        )
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+        })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id != 0L) {
+                binding.content?.let {
+                    with(it) {
+                        setText(post.content)
+                        AndroidUtils.showKeyboard(this)
+                    }
+                }
+            }
+        }
+
+        binding.save?.setOnClickListener {
+            binding.content?.let { editText ->
+                if (editText.text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        this@MainActivity.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.save(editText.text.toString())
+
+                editText.setText("")
+                editText.clearFocus()
+                AndroidUtils.hideKeyboard(editText)
+            }
         }
     }
 }
