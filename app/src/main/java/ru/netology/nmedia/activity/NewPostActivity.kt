fbdd147832d8ed782b88052c5cb8253a378ff327
Dmitry Sheleg <dimdim1999@mail.ru>
2026-06-09 16:20:48 +0300

    Create and Edit post in the same activity

--- app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -1,6 +1,5 @@
 package ru.netology.nmedia.activity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -10,30 +9,59 @@ import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.databinding.ActivityNewPostBinding
 import ru.netology.nmedia.util.AndroidUtils
 
-
 class NewPostActivity : AppCompatActivity() {
+
+    private companion object {
+        private const val EXTRA_ID = "extra_id"
+        private const val EXTRA_AUTHOR = "extra_author"
+        private const val EXTRA_CONTENT = "extra_content"
+        private const val EXTRA_PUBLISHED = "extra_published"
+        private const val EXTRA_LIKES = "extra_likes"
+        private const val EXTRA_LIKED_BY_ME = "extra_liked_by_me"
+        private const val EXTRA_SHARES = "extra_shares"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val binding = ActivityNewPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             WindowInsetsCompat.CONSUMED
         }
 
+        val id = intent.getLongExtra(EXTRA_ID, 0L)
+        val author = intent.getStringExtra(EXTRA_AUTHOR) ?: ""
+        val content = intent.getStringExtra(EXTRA_CONTENT) ?: ""
+        val published = intent.getStringExtra(EXTRA_PUBLISHED) ?: ""
+        val likes = intent.getIntExtra(EXTRA_LIKES, 0)
+        val likedByMe = intent.getBooleanExtra(EXTRA_LIKED_BY_ME, false)
+        val shares = intent.getIntExtra(EXTRA_SHARES, 0)
+
+        binding.edit.setText(content)
         AndroidUtils.showKeyboard(binding.edit)
+
         binding.ok.setOnClickListener {
-            if (binding.edit.text.isNullOrBlank()) {
-                setResult(Activity.RESULT_CANCELED)
-            } else {
-                val intent = Intent()
-                val content = binding.edit.text.toString()
-                intent.putExtra(Intent.EXTRA_TEXT, content)
-                setResult(Activity.RESULT_OK, intent)
+            val newContent = binding.edit.text.toString().trim()
+            if (newContent.isEmpty()) {
+                setResult(RESULT_CANCELED)
+                finish()
+                return@setOnClickListener
             }
+
+            val resultIntent = Intent().apply {
+                putExtra(EXTRA_ID, id)
+                putExtra(EXTRA_AUTHOR, author)
+                putExtra(EXTRA_CONTENT, newContent)
+                putExtra(EXTRA_PUBLISHED, published)
+                putExtra(EXTRA_LIKES, likes)
+                putExtra(EXTRA_LIKED_BY_ME, likedByMe)
+                putExtra(EXTRA_SHARES, shares)
+            }
+            setResult(RESULT_OK, resultIntent)
             finish()
         }
     }
