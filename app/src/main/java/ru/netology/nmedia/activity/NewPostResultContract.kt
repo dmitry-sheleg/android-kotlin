a83dee96cc8c3610f4afbf5fc3575b51ff6fb678
Dmitry Sheleg <dimdim1999@mail.ru>
2026-06-10 22:16:09 +0300

    Added VideoLink RUTUBE open by app or browser

--- app/src/main/java/ru/netology/nmedia/activity/NewPostResultContract.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostResultContract.kt
@@ -6,43 +6,6 @@ import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 import ru.netology.nmedia.dto.Post
 
-//object NewPostResultContract : ActivityResultContract<Post, Post?>() {
-//
-//    private const val EXTRA_ID = "extra_id"
-//    private const val EXTRA_AUTHOR = "extra_author"
-//    private const val EXTRA_CONTENT = "extra_content"
-//    private const val EXTRA_PUBLISHED = "extra_published"
-//    private const val EXTRA_LIKES = "extra_likes"
-//    private const val EXTRA_LIKED_BY_ME = "extra_liked_by_me"
-//    private const val EXTRA_SHARES = "extra_shares"
-//
-//    override fun createIntent(context: Context, input: Post): Intent =
-//        Intent(context, NewPostActivity::class.java).apply {
-//            putExtra(EXTRA_ID, input.id)
-//            putExtra(EXTRA_AUTHOR, input.author)
-//            putExtra(EXTRA_CONTENT, input.content)
-//            putExtra(EXTRA_PUBLISHED, input.published)
-//            putExtra(EXTRA_LIKES, input.likes)
-//            putExtra(EXTRA_LIKED_BY_ME, input.likedByMe)
-//            putExtra(EXTRA_SHARES, input.shares)
-//        }
-//
-//    override fun parseResult(resultCode: Int, intent: Intent?): Post? =
-//        if (resultCode == Activity.RESULT_OK && intent != null) {
-//            Post(
-//                id = intent.getLongExtra(EXTRA_ID, 0L),
-//                author = intent.getStringExtra(EXTRA_AUTHOR) ?: "",
-//                content = intent.getStringExtra(EXTRA_CONTENT) ?: "",
-//                published = intent.getStringExtra(EXTRA_PUBLISHED) ?: "",
-//                likes = intent.getIntExtra(EXTRA_LIKES, 0),
-//                likedByMe = intent.getBooleanExtra(EXTRA_LIKED_BY_ME, false),
-//                shares = intent.getIntExtra(EXTRA_SHARES, 0)
-//            )
-//        } else {
-//            null
-//        }
-//}
-
 object NewPostResultContract : ActivityResultContract<Post, Post?>() {
 
     private const val EXTRA_ID = "extra_id"
