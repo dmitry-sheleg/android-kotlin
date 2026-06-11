8913d1f5cefad5e97db2021cd59e5adcb802f52e
Dmitry Sheleg <dimdim1999@mail.ru>
2026-06-11 09:34:23 +0300

    Added storage in the posts.json

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySharedPrefsImpl.kt
@@ -0,0 +1,89 @@
+package ru.netology.nmedia.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.dto.Post
+import androidx.core.content.edit
+
+class PostRepositorySharedPrefsImpl(
+    context: Context,
+) : PostRepository {
+
+    private val prefs = context.getSharedPreferences("repo", Context.MODE_PRIVATE)
+    private var nextId = 1L
+    private var posts = emptyList<Post>()
+        set(value) {
+            field = value
+            sync()
+        }
+    private val data = MutableLiveData(posts)
+
+    init {
+        prefs.getString(KEY_POSTS, null)?.let { value ->
+            posts = gson.fromJson(value, type)
+            nextId = (posts.maxOfOrNull { it.id } ?: 0) + 1
+            data.value = posts
+        }
+    }
+
+    override fun getAll(): LiveData<List<Post>> = data
+
+    override fun save(post: Post) {
+        if (post.id == 0L) {
+            // TODO: remove hardcoded author & published
+            posts = listOf(
+                post.copy(
+                    id = nextId++,
+                    author = "Me",
+                    likedByMe = false,
+                    published = "now"
+                )
+            ) + posts
+            data.value = posts
+            return
+        }
+
+        posts = posts.map {
+            if (it.id != post.id) it else it.copy(content = post.content, video = post.video)
+        }
+        data.value = posts
+    }
+
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(shares = it.shares + 1)
+        }
+        data.value = posts
+    }
+
+    override fun removeById(id: Long) {
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+
+    private fun sync() {
+        prefs.edit {
+            putString(KEY_POSTS, gson.toJson(posts))
+        }
+    }
+
+    companion object {
+        private const val KEY_POSTS = "posts"
+
+        private val gson = Gson()
+        private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    }
+}
