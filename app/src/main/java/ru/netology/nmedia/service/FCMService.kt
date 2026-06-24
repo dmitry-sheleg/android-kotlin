2a00071d4d18e3f87acc1185d540f57b4be4c230
Dmitry Sheleg <dimdim1999@mail.ru>
2026-06-24 15:55:21 +0300

    Base functions for pushes added

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -1,14 +1,86 @@
 package ru.netology.nmedia.service
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import ru.netology.nmedia.R
+import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
+    private companion object {
+        const val ACTION_KEY = "action"
+        const val CONTENT_KEY = "content"
+        const val CHANNEL_ID = "remote"
+        val gson = Gson()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        message.data[ACTION_KEY]?.let {
+            when (Action.valueOf(it)) {
+                Action.LIKE -> handleLike(gson.fromJson(message.data[CONTENT_KEY], Like::class.java))
+            }
+        }
+    }
+
     override fun onNewToken(token: String) {
         println(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        println(message)
+    private fun handleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
     }
+
+    private fun notify(notification: Notification) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ||
+            checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+        }
+    }
+
 }
+
+enum class Action {
+    LIKE,
+}
+
+data class Like(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String,
+)
